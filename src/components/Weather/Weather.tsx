253c08ae1dc71a372db52c658c4263ea94923112
Guy Lillico <guylillico@gmail.com>
2022-07-25 16:36:21 +0800

    Context working in Weather component

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,7 +1,11 @@
 import React from "react"
+import { WeatherContext } from "../../providers/WeatherProvider"
 import { Wrapper } from "./Weather.styled"
 
 const Weather: React.FC = () => {
+  const { openWeatherData } = React.useContext(WeatherContext)
+  console.log(openWeatherData)
+
   return <Wrapper>Weather</Wrapper>
 }
 
