da8d8b4f21320be43bf817187b14c6da960c181c
Guy Lillico <guylillico@gmail.com>
2022-07-25 16:15:12 +0800

    Initial commit

---
+++ src/components/Weather/Weather.styled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components/macro"
+
+export const Wrapper = styled.div`
+  font-size: 16px;
+  text-align: center;
+  padding: 20px;
+  margin: 20px auto;
+  font-family: "Roboto", Arial, sans-serif;
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fcfcfc;
+  border-radius: 4px;
+`
---
+++ src/components/Weather/Weather.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Wrapper } from "./Weather.styled"
+
+const Weather = () => {
+  return <Wrapper>Weather</Wrapper>
+}
+
+export default Weather
