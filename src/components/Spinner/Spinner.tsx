e199a23dd788abc4a74629f07cf7f65c653a2b80
Guy Lillico <guylillico@gmail.com>
2022-07-25 16:16:19 +0800

    Added Spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import { SpinnerOverlay, SpinnerContainer } from "./Spinner.styled"
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+)
+
+export default Spinner
